b6fff49be0d2788ff9125cb44bc75d6262af3898
Dolgorykij <dolgo-rykij@mail.ru>
2024-03-26 21:50:29 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ну здравствуй,дивный новый мир");
+        System.out.println("Brave new world");
     }
 }
